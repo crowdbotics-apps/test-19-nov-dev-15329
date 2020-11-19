bcb662df9f5f993e8173e260ec26d26e354e2706
Crowdbotics <team@crowdbotics.com>
2020-11-19 14:25:19 +0000

    Update components

--- src/features/BlankScreen114708/screens/blank.js
+++ src/features/BlankScreen114708/screens/blank.js
@@ -27,22 +27,27 @@ export default class Blank extends React.Component {
 
   render = () => (
     <View>
-      <Text>Sample text content</Text>
+      <Text style={styles.Text_2}>Sample text content</Text>
       <TextInput
         placeholder="Sample text input placeholder"
+        style={styles.TextInput_3}
         value={this.state.TextInput_3}
         onChangeText={nextValue => this.setState({ TextInput_3: nextValue })}
       />
-      <Button title="Press me!" onPress={() => alert("Pressed!")} />
+      <Button
+        title="Press me!"
+        style={styles.Button_4}
+        onPress={() => alert("Pressed!")}
+      />
       <TextInput
         placeholder="Number Input Placeholder"
-        keyboardType="numeric"
+        style={styles.TextInput_5}
         value={this.state.TextInput_5}
         onChangeText={nextValue => this.setState({ TextInput_5: nextValue })}
       />
       <TextInput
         placeholder="Sample text input placeholder"
-        multiline={true}
+        style={styles.TextInput_6}
         value={this.state.TextInput_6}
         onChangeText={nextValue => this.setState({ TextInput_6: nextValue })}
       />
@@ -55,10 +60,52 @@ const styles = StyleSheet.create({
     flex: 1,
     marginHorizontal: 16
   },
+
   View_1: {},
   Text_2: {},
   TextInput_3: {},
   Button_4: {},
   TextInput_5: {},
-  TextInput_6: {}
+  TextInput_6: {},
+  View_1: {},
+  Text_2: {
+    width: "100%",
+    borderWidth: 3,
+    borderLeftWidth: 3,
+    borderRightWidth: 3,
+    borderTopWidth: 3,
+    borderBottomWidth: 3,
+    textAlign: "right"
+  },
+  TextInput_3: {
+    borderWidth: 3,
+    borderLeftWidth: 3,
+    borderRightWidth: 3,
+    borderTopWidth: 3,
+    borderBottomWidth: 3
+  },
+  Button_4: {
+    borderWidth: 4,
+    borderLeftWidth: 4,
+    borderRightWidth: 4,
+    borderTopWidth: 4,
+    borderBottomWidth: 4
+  },
+  TextInput_5: {
+    borderWidth: 3,
+    borderLeftWidth: 3,
+    borderRightWidth: 3,
+    borderTopWidth: 3,
+    borderBottomWidth: 3,
+    textAlign: "right"
+  },
+  TextInput_6: {
+    height: 100,
+    borderWidth: 3,
+    borderLeftWidth: 3,
+    borderRightWidth: 3,
+    borderTopWidth: 3,
+    borderBottomWidth: 3,
+    textAlign: "center"
+  }
 })
